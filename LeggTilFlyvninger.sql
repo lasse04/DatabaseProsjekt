d46094ed637541ac94c58155f11560cc58fe4118
Lasse Ranum <lasseran@stud.ntnu.no>
2025-03-12 09:56:00 +0100

    Lagt til brukertilfelle 4

---
+++ LeggTilFlyvninger.sql
@@ -0,0 +1,3 @@
+INSERT INTO Flyvning (flyruteNr, løpeNr, dato) VALUES ('WF1302', 1, '2025-04-01');
+INSERT INTO Flyvning (flyruteNr, løpeNr, dato) VALUES ('DY753', 1, '2025-04-01');
+INSERT INTO Flyvning (flyruteNr, løpeNr, dato) VALUES ('SK888', 1, '2025-04-01');
